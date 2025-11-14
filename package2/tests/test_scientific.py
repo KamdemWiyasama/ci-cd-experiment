510dde74dab6982fcecf47f6212032f5443f2a54
Loryann <loryannwiyasama@gmail.com>
2025-11-14 14:07:25 -0500

    Corrected commit message:

--- package2/tests/test_scientific.py
+++ package2/tests/test_scientific.py
@@ -4,9 +4,8 @@ ITERATIONS = 1_000_000
 
 def test_power():
     for _ in range(ITERATIONS):
-        assert test_scientific.power(2, 3) == 8
+        assert scientific.power(2, 3) == 8
 
 def test_square_root():
     for _ in range(ITERATIONS):
-        assert test_scientific.square_root(16) == 4
-
+        assert scientific.square_root(16) == 4

0629c537e973c9b8dff2c03ecf416a2c088aac8a
Loryann <loryannwiyasama@gmail.com>
2025-11-14 13:58:39 -0500

    Changed attributes for square_root and power functions

--- package2/tests/test_scientific.py
+++ package2/tests/test_scientific.py
@@ -2,11 +2,11 @@ from package2.tests import test_scientific
 
 ITERATIONS = 1_000_000
 
-def test_power():
+def power():
     for _ in range(ITERATIONS):
         assert test_scientific.power(2, 3) == 8
 
-def test_square_root():
+def square_root():
     for _ in range(ITERATIONS):
         assert test_scientific.square_root(16) == 4
 
